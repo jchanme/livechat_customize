cb0512928e54c3854acfe2f87087bbf676c99776
jchanme <39323414+jchanme@users.noreply.github.com>
2018-05-26 17:08:48 +0800

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Livechat Customize",
     "version": "11.0.1.0.1",
-    "depends": ['im_livechat']                ],
+    "depends": ['im_livechat'],
     'author': 'Safyric Co., Ltd.',
     "category": "Localization",
     'license': 'AGPL-3',
