9f03b181f7a4a6ddd466cdd7dd7e81406373d814
jchanme <39323414+jchanme@users.noreply.github.com>
2018-05-26 16:54:49 +0800

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Livechat Customize",
+    "version": "11.0.1.0.1",
+    "depends": ['im_livechat']                ],
+    'author': 'Safyric Co., Ltd.',
+    "category": "Localization",
+    'license': 'AGPL-3',
+    'installable': True,
+}
